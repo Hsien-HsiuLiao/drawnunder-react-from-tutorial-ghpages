89ee11665596efad0a2e643305a89a4eda7eddb4
Hsien <hsien-hsiu.liao@yandex.com>
2020-03-28 21:15:41 -0700

    Updates

---
+++ precache-manifest.8e0bb54548e554571c9c185986198d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56bdbb0ae687c0aaffa872e6e8215d2",
+    "url": "/drawnunder-react/index.html"
+  },
+  {
+    "revision": "ac2fe4e4110601946598",
+    "url": "/drawnunder-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bba85f3997c82cb3499",
+    "url": "/drawnunder-react/static/js/2.a5bbf318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drawnunder-react/static/js/2.a5bbf318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2fe4e4110601946598",
+    "url": "/drawnunder-react/static/js/main.36d8b539.chunk.js"
+  },
+  {
+    "revision": "cc4415089a0c71d3f12e",
+    "url": "/drawnunder-react/static/js/runtime-main.27aa6510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drawnunder-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
